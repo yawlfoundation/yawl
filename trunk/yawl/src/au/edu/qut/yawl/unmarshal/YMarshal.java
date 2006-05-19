dcc81d59cafc75553b5e9a160c372629b949539a
nathan_rose <nathan_rose@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-05-19 16:28:02 +0000

    refactored unmarshalSpecifications and added a version that takes the XML as a string

--- trunk/yawl/src/au/edu/qut/yawl/unmarshal/YMarshal.java
+++ trunk/yawl/src/au/edu/qut/yawl/unmarshal/YMarshal.java
@@ -9,23 +9,27 @@
 
 package au.edu.qut.yawl.unmarshal;
 
-import au.edu.qut.yawl.elements.YSpecification;
-import au.edu.qut.yawl.exceptions.YSchemaBuildingException;
-import au.edu.qut.yawl.exceptions.YSyntaxException;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
-
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.xml.sax.InputSource;
+
+import au.edu.qut.yawl.elements.YSpecification;
+import au.edu.qut.yawl.exceptions.YSchemaBuildingException;
+import au.edu.qut.yawl.exceptions.YSyntaxException;
 
 /**
  * 
@@ -59,6 +63,27 @@ public class YMarshal {
         return specifications;
     }
 
+    /**
+     * Returns the _specifications.   Does some primary checking of the XML against
+     * schemas and checks well formedness of the XML.
+     * @return List
+     */
+    public static List unmarshalSpecifications( String specificationSetXMLString, String specificationSetID )
+    		throws YSyntaxException, YSchemaBuildingException, JDOMException, IOException {
+        //first check if is well formed and build a document
+    	Document document = buildSpecificationSetDocument(
+    			new InputSource( new StringReader( specificationSetXMLString ) ) );
+    	
+        //next get the version out as text
+    	String version = getSpecificationSetVersion( document );
+    	
+        //now check the specification file against its' respective schema
+    	validateSpecification( new InputSource( new StringReader( specificationSetXMLString ) ),
+    			specificationSetID, version );
+    	
+        //now build a set of specifications - verification has not yet occured.
+    	return buildSpecifications( document );
+    }
 
     /**
      * Returns the _specifications.   Does some primary checking of the file against
@@ -67,29 +92,70 @@ public class YMarshal {
      */
     public static List unmarshalSpecifications(String specificationSetFileID)
             throws YSyntaxException, YSchemaBuildingException, JDOMException, IOException {
+    	File file = new File( specificationSetFileID );
+    	
         //first check if is well formed and build a document
-        SAXBuilder builder = new SAXBuilder();
-        Document document = builder.build(specificationSetFileID);
-        Element specificationSetEl = document.getRootElement();
-
+    	Document document = buildSpecificationSetDocument( new InputSource( new FileInputStream( file ) ) );
+    	
+        //next get the version out as text
+    	String version = getSpecificationSetVersion( document );
+    	
+        //now check the specification file against its' respective schema
+    	validateSpecification( new InputSource( new FileInputStream( file ) ),
+    			specificationSetFileID, version );
+    	
+        //now build a set of specifications - verification has not yet occured.
+    	return buildSpecifications( document );
+    }
+    
+    private static Document buildSpecificationSetDocument( InputSource specificationSetSource )
+    		throws JDOMException, IOException {
+        //first check if is well formed and build a document
+    	SAXBuilder builder = new SAXBuilder();
+    	return builder.build( specificationSetSource );
+    }
+    
+    /**
+     * @return the beta version of the specification set.
+     */
+    private static String getSpecificationSetVersion( Document specificationSetDocument ) {
+    	Element specificationSetEl = specificationSetDocument.getRootElement();
+    	
         //next get the version out as text - if possible
-        String version = specificationSetEl.getAttributeValue("version");
-        if (null == version) {
+    	String version = specificationSetEl.getAttributeValue( "version" );
+    	if( null == version ) {
             //version attribute was not mandatory in version 2
             //therefore a missing version number would likely be version 2
             version = YSpecification._Beta2;
-        }
-
+    	}
+    	return version;
+    }
+    
+    /**
+     * @param specificationSetSource an InputSource from which the specification set can be read.
+     * @param specificationSetID an identifier that will appear in the exception if validation fails
+     *                           to indicate which specification set failed validation (optional).
+     * @param version which version schema to check against.
+     * @throws YSyntaxException if the specification set doesn't validate against the schema
+     *                          for the specified beta version.
+     */
+    private static void validateSpecification( InputSource specificationSetSource,
+    		String specificationSetID, String version ) throws YSyntaxException {
         //now check the specification file against its' respective schema
-        String errors = YawlXMLSpecificationValidator.getInstance().checkSchema(specificationSetFileID, version);
-        if (errors == null || errors.length() > 0) {
-            throw new YSyntaxException(
-                    " The file [" + specificationSetFileID + "] failed to verify against YAWL's Schema:\n"
-                    + errors);
-        }
-
-        //now build a set of specifications - verification has not yet occured.
-        return buildSpecifications(document);
+    	String errors = YawlXMLSpecificationValidator.getInstance().checkSchema(
+    			specificationSetSource, version );
+    	if( errors == null || errors.length() > 0 ) {
+    		if( specificationSetID != null ) {
+    			throw new YSyntaxException(
+    					" The specification set [" + specificationSetID +
+                    	"] failed to verify against YAWL's Schema:\n" + errors);
+    		}
+    		else {
+    			throw new YSyntaxException(
+                        " The specification set failed to verify against YAWL's Schema:\n"
+                        + errors);
+    		}
+    	}
     }
 
 
