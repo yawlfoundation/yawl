7f916a1070f26ecad3ddbdb67cfa34f32b399eb9
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-05-31 18:43:35 +0000

    more functionality for managed data tests

--- trunk/yawl/test/au/edu/qut/yawl/persistence/managed/TestDataContext.java
+++ trunk/yawl/test/au/edu/qut/yawl/persistence/managed/TestDataContext.java
@@ -31,7 +31,26 @@ public class TestDataContext extends TestCase implements VetoableChangeListener{
 		DataProxy dp = dc.newObject(YSpecification.class, this);
 		assertNotNull(dp);
 		YSpecification spec = (YSpecification) dp.getData();
+		spec.setName("aTestName");
 		assertNotNull(spec);
+		DataProxy dp2 = dc.getDataProxy(spec);
+		assertEquals(dp, dp2);
+		Object o = dc.getKeyFor(dp);
+		assertNotNull(o);
+		assertEquals(o, spec.getName());
+		lastEvent = null;
+		try {
+			dp.setAttribute("name", "aNewName");
+		}
+		catch(Exception e) {fail("should have been able to set attribute");}
+		assertEquals(lastEvent.getSource(), spec);
+		assertEquals(lastEvent.getPropertyName(), "name");
+		assertEquals(lastEvent.getOldValue(), "aTestName");
+		assertEquals(lastEvent.getNewValue(), "aNewName");
+		dc.put(dp);
+		dc.remove(dp);
+		dc.remove(dp);
+
 	}
 	
 }
