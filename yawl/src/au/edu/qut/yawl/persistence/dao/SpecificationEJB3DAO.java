f4ab1a216173e18bd34e432929a819030b4e3218
nathan_rose <nathan_rose@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-06-06 23:09:55 +0000

    uploaded EJB3 DAO and a jar for EJB3

---
+++ yawl/src/au/edu/qut/yawl/persistence/dao/SpecificationEJB3DAO.java
@@ -0,0 +1,43 @@
+/*
+ * This file is made available under the terms of the LGPL licence.
+ * This licence can be retreived from http://www.gnu.org/copyleft/lesser.html.
+ * The source remains the property of the YAWL Foundation.  The YAWL Foundation is a collaboration of
+ * individuals and organisations who are commited to improving workflow technology.
+ *
+ */
+
+package au.edu.qut.yawl.persistence.dao;
+import java.io.Serializable;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import au.edu.qut.yawl.elements.YSpecification;
+
+@Stateless
+@Remote(SpecificationDAO.class)
+public class SpecificationEJB3DAO implements SpecificationDAO {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	public boolean delete( YSpecification t ) {
+		manager.remove( t );
+		return true;
+	}
+
+	public Serializable getKey( YSpecification t ) {
+		return t.getDbID();
+	}
+
+	public YSpecification retrieve( Object key ) {
+		return manager.find(YSpecification.class, key);
+	}
+
+	public int save( YSpecification t ) {
+		manager.merge( t );
+		return 0;
+	}
+}
