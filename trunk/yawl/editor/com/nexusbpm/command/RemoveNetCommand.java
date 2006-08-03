6bf33cd5d7efad35b1da2d6c7ab8b1c13c5d19d1
nathan_rose <nathan_rose@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-08-03 21:21:39 +0000

    fixed the command to remove a net

--- trunk/yawl/editor/com/nexusbpm/command/RemoveNetCommand.java
+++ trunk/yawl/editor/com/nexusbpm/command/RemoveNetCommand.java
@@ -7,65 +7,148 @@
  */
 package com.nexusbpm.command;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import au.edu.qut.yawl.elements.YCompositeTask;
+import operation.WorkflowOperation;
 import au.edu.qut.yawl.elements.YDecomposition;
 import au.edu.qut.yawl.elements.YExternalNetElement;
+import au.edu.qut.yawl.elements.YFlow;
 import au.edu.qut.yawl.elements.YNet;
 import au.edu.qut.yawl.elements.YSpecification;
-
-import com.nexusbpm.editor.persistence.EditorDataProxy;
+import au.edu.qut.yawl.elements.YTask;
+import au.edu.qut.yawl.persistence.managed.DataContext;
+import au.edu.qut.yawl.persistence.managed.DataProxy;
 
 /**
- * The RemoveNetCommand removes a YNet from a YSpecification
+ * The RemoveNetCommand removes a YNet from a YSpecification. All of the
+ * net elements inside the net and the decompositions that those net
+ * elements decompose to (except the decompositions that net elements of
+ * other nets decompose to) and the flows connected to the net's net
+ * elements are all disconnected from the data context.
  * 
  * @author Matthew Sandoz
- *
+ * @author Nathan Rose
  */
-public class RemoveNetCommand implements Command {
-
-	EditorDataProxy netToRemove;
-	
-	public RemoveNetCommand(EditorDataProxy proxy) {
-		netToRemove = proxy;
-	}
-	
-	public void execute() {
-		YNet net = (YNet) netToRemove.getData();
-		YSpecification spec = net.getParent();
-		List<YDecomposition> decomps = spec.getDecompositions();
-		decomps.remove(net);
-		netToRemove.getContext().delete(netToRemove);
-		for (YDecomposition decomp: decomps) {
-			if (decomp instanceof YNet) {
-				net = (YNet) decomp;
-				List<YExternalNetElement> elements = net.getNetElements();
-				for (YExternalNetElement element: elements) {
-					if (element instanceof YCompositeTask) {
-						YCompositeTask task = (YCompositeTask) element;
-						if (task.getDecompositionPrototype() == net) {
-							task.setDecompositionPrototype(null);
-							//so we dont have to remove the composite task...
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	public void undo() {
-//		YNet net = (YNet) netToRemove.getData();
-        throw new UnsupportedOperationException(
-                "undo not yet implemented");
-	}
+public class RemoveNetCommand extends AbstractCommand {
+	private DataProxy<YNet> netProxy;
+    private YNet net;
     
-    public void redo() {
-        throw new UnsupportedOperationException(
-                "redo not yet implemented");
+    private DataContext context;
+    
+    private YSpecification parentSpec;
+    private DataProxy<YSpecification> parentSpecProxy;
+    
+    private Map<YFlow, DataProxy> flows;
+    private Map<YFlow, DataProxy> flowParents;
+    private Map<YExternalNetElement, DataProxy> netElements;
+    private Map<YDecomposition, DataProxy> decompositions;
+	
+	public RemoveNetCommand( DataProxy netProxy ) {
+		this.netProxy = netProxy;
+        context = netProxy.getContext();
+	}
+	
+	/**
+     * @see com.nexusbpm.command.AbstractCommand#attach()
+     */
+    @Override
+    protected void attach() throws Exception {
+        for( YFlow flow : flows.keySet() ) {
+            DataProxy proxy = flows.get( flow );
+            DataProxy parentProxy = flowParents.get( flow );
+            context.detachProxy( proxy, flow, parentProxy );
+        }
+        for( YExternalNetElement element : netElements.keySet() ) {
+            DataProxy proxy = netElements.get( element );
+            context.detachProxy( proxy, element, netProxy );
+        }
+        for( YDecomposition decomp : decompositions.keySet() ) {
+            DataProxy proxy = decompositions.get( decomp );
+            WorkflowOperation.detachDecompositionFromSpec( decomp );
+            context.detachProxy( proxy, decomp, parentSpecProxy );
+        }
     }
     
-    public boolean supportsUndo() {
-        return false;
+    /**
+     * @see com.nexusbpm.command.AbstractCommand#detach()
+     */
+    @Override
+    protected void detach() throws Exception {
+        for( YDecomposition decomp : decompositions.keySet() ) {
+            DataProxy proxy = decompositions.get( decomp );
+            WorkflowOperation.attachDecompositionToSpec( decomp, parentSpec );
+            context.attachProxy( proxy, decomp, parentSpecProxy );
+        }
+        for( YExternalNetElement element : netElements.keySet() ) {
+            DataProxy proxy = netElements.get( element );
+            context.attachProxy( proxy, element, netProxy );
+        }
+        for( YFlow flow : flows.keySet() ) {
+            DataProxy proxy = flows.get( flow );
+            DataProxy parentProxy = flowParents.get( flow );
+            context.attachProxy( proxy, flow, parentProxy );
+        }
+    }
+    
+    /**
+     * @see com.nexusbpm.command.AbstractCommand#perform()
+     */
+    @Override
+    protected void perform() throws Exception {
+        net = netProxy.getData();
+        parentSpec = net.getParent();
+        assert parentSpec != null : "parent specification was null";
+        parentSpecProxy = context.getDataProxy( parentSpec, null );
+        assert parentSpecProxy != null : "parent specification's proxy was null";
+        
+        // set of decompositions used by the tasks in other nets
+        Set<YDecomposition> usedDecomps = new HashSet<YDecomposition>();
+        
+        for( YDecomposition decomp : parentSpec.getDecompositions() ) {
+            if( decomp instanceof YNet && net != decomp ) {
+                for( YExternalNetElement element : ((YNet) decomp).getNetElements() ) {
+                    if( element instanceof YTask ) {
+                        YDecomposition d = ((YTask) element).getDecompositionPrototype();
+                        if( d != null ) {
+                            usedDecomps.add( d );
+                        }
+                    }
+                }
+            }
+        }
+        
+        DataProxy proxy;
+        
+        flows = new HashMap<YFlow, DataProxy>();
+        flowParents = new HashMap<YFlow, DataProxy>();
+        netElements = new HashMap<YExternalNetElement, DataProxy>();
+        decompositions = new HashMap<YDecomposition, DataProxy>();
+        
+        for( YExternalNetElement element : net.getNetElements() ) {
+            proxy = context.getDataProxy( element, null );
+            assert proxy != null : "net element's proxy was null";
+            netElements.put( element, proxy );
+            if( element instanceof YTask ) {
+                YTask task = (YTask) element;
+                YDecomposition decomp = task.getDecompositionPrototype();
+                if( decomp != null && !usedDecomps.contains( decomp ) ) {
+                    proxy = context.getDataProxy( decomp, null );
+                    assert proxy != null : "decomposition's proxy was null";
+                    decompositions.put( decomp, proxy );
+                }
+            }
+            for( YFlow flow : element.getPostsetFlows() ) {
+                proxy = context.getDataProxy( flow, null );
+                assert proxy != null : "flow's proxy was null";
+                flows.put( flow, proxy );
+                proxy = context.getDataProxy( flow.getParent(), null );
+                assert proxy != null : "flow's parent's proxy was null";
+                flowParents.put( flow, proxy );
+            }
+        }
+        decompositions.put( net, netProxy );
     }
 }
