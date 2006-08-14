2adf35cd7b1601c633c0f0bdbc1c78d324d72942
deanmao <deanmao@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-08-14 21:24:03 +0000

    added adapter class

---
+++ yawl/src/au/edu/qut/yawl/persistence/managed/DataProxyStateChangeAdapter.java
@@ -0,0 +1,27 @@
+package au.edu.qut.yawl.persistence.managed;
+
+import java.beans.PropertyChangeEvent;
+
+
+public class DataProxyStateChangeAdapter implements DataProxyStateChangeListener {
+
+	public void proxyAttached( DataProxy proxy, Object data, DataProxy parent ) {
+
+	}
+
+	public void proxyAttaching( DataProxy proxy, Object data, DataProxy parent ) {
+
+	}
+
+	public void proxyDetached( DataProxy proxy, Object data, DataProxy parent ) {
+
+	}
+
+	public void proxyDetaching( DataProxy proxy, Object data, DataProxy parent ) {
+
+	}
+
+	public void propertyChange( PropertyChangeEvent evt ) {
+	}
+
+}
