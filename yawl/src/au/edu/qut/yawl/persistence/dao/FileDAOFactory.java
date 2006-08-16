9eaac268da81bb63aa89df5cf2d802ad6362bf36
nathan_rose <nathan_rose@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-08-16 21:23:42 +0000

    removed old dao

--- yawl/src/au/edu/qut/yawl/persistence/dao/FileDAOFactory.java
+++
@@ -1,9 +0,0 @@
-package au.edu.qut.yawl.persistence.dao;
-
-public class FileDAOFactory extends DAOFactory {
-	@Override
-	public SpecificationDAO getSpecificationModelDAO() {
-		return new SpecificationFileDAO();
-	}
-}
- 
