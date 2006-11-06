a8b0d0c460beaefd78e5e447540418ec767b78a2
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-06 16:05:58 +0000

    batch of license header additions

--- yawl/editor/com/nexusbpm/editor/logger/LogRecordListener.java
+++ yawl/editor/com/nexusbpm/editor/logger/LogRecordListener.java
@@ -1,3 +1,11 @@
+/*
+ * This file is made available under the terms of the LGPL licence.
+ * This licence can be retreived from http://www.gnu.org/copyleft/lesser.html.
+ * The source remains the property of the YAWL Group.  The YAWL Group is a collaboration of 
+ * individuals and organiations who are commited to improving workflow technology.
+ *
+ */
+
 package com.nexusbpm.editor.logger;
 
 import au.edu.qut.yawl.util.ObjectMessageListener;
