5f39fb67f20587bac0e2fa8cfb6744c04b7fd674
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-07 22:22:48 +0000

    changes to support moving jms out of yawl app

--- trunk/yawl/src/au/edu/qut/yawl/events/JmsProvider.java
+++ trunk/yawl/src/au/edu/qut/yawl/events/JmsProvider.java
@@ -8,16 +8,8 @@
 
 package au.edu.qut.yawl.events;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Serializable;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.ResourceBundle;
 
@@ -29,42 +21,19 @@ import javax.jms.ObjectMessage;
 import javax.jms.Session;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
-import org.apache.log4j.xml.DOMConfigurator;
-import org.exolab.castor.xml.MarshalException;
-import org.exolab.castor.xml.ValidationException;
-import org.exolab.jms.administration.AdminConnectionFactory;
-import org.exolab.jms.administration.JmsAdminServerIfc;
-import org.exolab.jms.config.Configuration;
-import org.exolab.jms.config.ConfigurationManager;
-import org.exolab.jms.config.ConfigurationReader;
-import org.exolab.jms.config.LoggerConfiguration;
-import org.exolab.jms.server.JmsServer;
-import org.exolab.jms.server.ServerException;
-import org.exolab.jms.service.ServiceException;
 
 public class JmsProvider {
 
-	private static final String OPENJMS_HOME = "openjms.home";
-
 	public static final String OPENJMS_YAWL_EVENTQUEUE = "openjms.yawl.eventqueue";
-
-	private static final String OPENJMS_ADMIN_PASSWORD = "openjms.admin.password";
-
-	private static final String OPENJMS_ADMIN_USER = "openjms.admin.user";
-
-	private static final String JAVA_NAMING_PROVIDER_URL = "java.naming.provider.url";
-
-	private JmsServer server;
 
 	private static JmsProvider INSTANCE;
 
 	private ResourceBundle bundle;
 
 	public static synchronized JmsProvider getInstance() {
-		if (INSTANCE == null)
+		if (INSTANCE == null) {
 			INSTANCE = new JmsProvider();
+		}
 		return INSTANCE;
 	}
 
@@ -76,90 +45,22 @@ public class JmsProvider {
 
 	private JmsProvider() {
 			bundle = ResourceBundle.getBundle("jndi");
-
+			Hashtable<String, String> properties = new Hashtable<String, String>();
 			try {
-				initServer();
-			} catch (Exception e1) {
-				e1.printStackTrace();
+				Enumeration<String> e = bundle.getKeys();
+			while(e.hasMoreElements()) {
+				String key = e.nextElement();
+				String val = bundle.getString(key);
+				properties.put(key, val);
 			}
-			try {
-
-			Hashtable properties = new Hashtable();
-			properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.exolab.jms.jndi.InitialContextFactory");
-			properties.put(Context.PROVIDER_URL, "tcp://localhost:3035/");
 
 			context = new InitialContext(properties);			
-			
-//			context = new InitialContext();
 			connection = getConnection(context);
+			context.getEnvironment();
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public final void initServer() throws NamingException, ServiceException,
-			InstantiationException, IllegalAccessException,
-			ClassNotFoundException, SQLException, IOException,
-			MarshalException, ValidationException {
-		System.setProperty(OPENJMS_HOME, getContextProperty(OPENJMS_HOME));
-		Class.forName("org.hsqldb.jdbcDriver").newInstance();
-		java.sql.Connection conn = DriverManager.getConnection(
-				"jdbc:hsqldb:mem:openjms", "sa", "");
-		String setupDb = readStreamAsString(this.getClass()
-				.getResourceAsStream("/create_hsql.sql"));
-		PreparedStatement s = conn.prepareStatement(setupDb);
-		s.execute();
-		Configuration config = null;
-		config = ConfigurationReader.read(this.getClass().getResourceAsStream(
-				"/openjms.xml"));
-//		config.getLoggerConfiguration().setFile(
-//				this.getClass().getResource("/log4j.properties").getPath());
-		server = new JmsServer(config) {
-		    public void init() throws NamingException, ServiceException {
-		    	try {
-		    		registerServices();
-		    		getServices().start();
-		    	} catch (ServiceException exception) {
-		    		throw new ServerException(
-		    				"Failed to start services", exception);
-		    		}
-		    	}			
-			};
-		server.init();
-	}
-
-	public void stopServer() throws Exception {
-		String url = getContextProperty(JAVA_NAMING_PROVIDER_URL).toString();
-		String user = getContextProperty(OPENJMS_ADMIN_USER);
-		String password = getContextProperty(OPENJMS_ADMIN_PASSWORD);
-		JmsAdminServerIfc admin = AdminConnectionFactory.create(url, user,
-				password);
-		Thread.sleep(5000);
-		admin.stopServer();
-		Thread.sleep(5000);
-		java.sql.Connection conn = DriverManager.getConnection(
-				"jdbc:hsqldb:mem:openjms", "sa", "");
-		conn.createStatement().execute("SHUTDOWN");
-	}
-
-	private static String readStreamAsString(InputStream file)
-			throws java.io.IOException {
-		StringBuffer fileData = new StringBuffer(1000);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
-		char[] buf = new char[1024];
-		int numRead = 0;
-		while ((numRead = reader.read(buf)) != -1) {
-			String readData = String.valueOf(buf, 0, numRead);
-			fileData.append(readData);
-			buf = new char[1024];
-		}
-		reader.close();
-		return fileData.toString();
-	}
-
-	private String getContextProperty(String name) throws NamingException {
-		return bundle.getString(name);
 	}
 
 	private Connection getConnection(Context context) throws Exception {
@@ -171,13 +72,13 @@ public class JmsProvider {
 
 	public MessageProducer getMessageProducer() throws Exception {
 		Destination dest = (Destination) context
-				.lookup(getContextProperty(OPENJMS_YAWL_EVENTQUEUE));
+				.lookup(bundle.getString(OPENJMS_YAWL_EVENTQUEUE));
 		MessageProducer sender = session.createProducer(dest);
 		connection.start();
 		return sender;
 	}
 
-	public ObjectMessage getObjectMessage(Serializable o) throws Exception {
+	public ObjectMessage createObjectMessage(Serializable o) throws Exception {
 		return session.createObjectMessage(o);
 	}
 
