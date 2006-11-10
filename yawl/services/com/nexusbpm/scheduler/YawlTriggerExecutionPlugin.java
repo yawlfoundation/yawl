575959d5bf13b6817b588118033a7a58fdb39747
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-10 14:28:05 +0000

    added new spring based capability to scheduler and created a quartz plugin to handle all the work

--- yawl/services/com/nexusbpm/scheduler/YawlTriggerExecutionPlugin.java
+++ yawl/services/com/nexusbpm/scheduler/YawlTriggerExecutionPlugin.java
@@ -1,18 +1,23 @@
 package com.nexusbpm.scheduler;
 
+import java.util.Date;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.TriggerListener;
 import org.quartz.spi.SchedulerPlugin;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class NexusWorkflowTriggerExecutionPlugin implements SchedulerPlugin,
+public class YawlTriggerExecutionPlugin implements SchedulerPlugin,
 		TriggerListener {
 
 	private String name;
-
-	public NexusWorkflowTriggerExecutionPlugin() {}
+	private String appContextUrl;
+	QuartzEventDao dao;
+	public YawlTriggerExecutionPlugin() {}
 
 	/**
 	 * <p>
@@ -26,10 +31,17 @@ public class NexusWorkflowTriggerExecutionPlugin implements SchedulerPlugin,
 	public void initialize(String name, Scheduler scheduler)
 			throws SchedulerException {
 		this.name = name;
-
+		String[] paths = {getAppContextUrl()};
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(paths);
+		dao = (QuartzEventDao) ctx.getBean("quartzDao");
 		scheduler.addGlobalTriggerListener(this);
 	}
 
+	protected void saveEvent(QuartzEvent qe) {
+		dao.saveRecord(qe);
+	}
+	
+	
 	public void start() {
 	}
 
@@ -53,6 +65,8 @@ public class NexusWorkflowTriggerExecutionPlugin implements SchedulerPlugin,
 				new java.util.Date(), context.getJobDetail().getName(),
 				context.getJobDetail().getGroup(),
 				new Integer(context.getRefireCount()) };
+		QuartzEvent qe = new QuartzEvent(trigger.getName(), trigger.getPreviousFireTime(), new Date(), null, "fired");
+		saveEvent(qe);
 	}
 
 	public void triggerMisfired(Trigger trigger) {
@@ -60,6 +74,8 @@ public class NexusWorkflowTriggerExecutionPlugin implements SchedulerPlugin,
 				trigger.getPreviousFireTime(), trigger.getNextFireTime(),
 				new java.util.Date(), trigger.getJobGroup(),
 				trigger.getJobGroup() };
+		QuartzEvent qe = new QuartzEvent(trigger.getName(), trigger.getPreviousFireTime(), new Date(), null, "misfired");
+		saveEvent(qe);
 	}
 
 	public void triggerComplete(Trigger trigger, JobExecutionContext context,
@@ -83,10 +99,20 @@ public class NexusWorkflowTriggerExecutionPlugin implements SchedulerPlugin,
 				context.getJobDetail().getGroup(),
 				new Integer(context.getRefireCount()),
 				new Integer(triggerInstructionCode), instrCode };
+		QuartzEvent qe = new QuartzEvent(trigger.getName(), trigger.getPreviousFireTime(), new Date(), context.getResult().toString(), "fired");
+		saveEvent(qe);
 	}
 
 	public boolean vetoJobExecution(Trigger trigger, JobExecutionContext context) {
 		return false;
+	}
+
+	public String getAppContextUrl() {
+		return appContextUrl;
+	}
+
+	public void setAppContextUrl(String appContextUrl) {
+		this.appContextUrl = appContextUrl;
 	}
 
 }
