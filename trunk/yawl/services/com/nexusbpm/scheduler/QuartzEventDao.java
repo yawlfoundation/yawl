72373f0ecbc066344691c254b84f5d09e3c319e1
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-10 18:56:53 +0000

    more functionality for scheduler cleaned up dao and plugin and added license text

--- trunk/yawl/services/com/nexusbpm/scheduler/QuartzEventDao.java
+++ trunk/yawl/services/com/nexusbpm/scheduler/QuartzEventDao.java
@@ -1,3 +1,11 @@
+/*
+ * This file is made available under the terms of the LGPL licence.
+ * This licence can be retreived from http://www.gnu.org/copyleft/lesser.html.
+ * The source remains the property of the YAWL Group.  The YAWL Group is a collaboration of 
+ * individuals and organiations who are commited to improving workflow technology.
+ *
+ */
+
 package com.nexusbpm.scheduler;
 
 import java.util.Date;
@@ -8,6 +16,5 @@ public interface QuartzEventDao {
     public QuartzEvent getRecord(Long id);
     public List getRecords(Date startDate, Date endDate);
     public void saveRecord(QuartzEvent record);
-
-public void removeRecord(Long id);
+    public void removeRecord(Long id);
 }
--- trunk/yawl/services/com/nexusbpm/scheduler/StartYawlCaseJob.java
+++ trunk/yawl/services/com/nexusbpm/scheduler/StartYawlCaseJob.java
@@ -9,66 +9,56 @@
 package com.nexusbpm.scheduler;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Date;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.quartz.Job;
-import org.quartz.JobDataMap;
-import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.Trigger;
-
 import au.edu.qut.yawl.engine.interfce.InterfaceB_EnvironmentBasedClient;
 
 import com.nexusbpm.editor.util.InterfaceB;
 
 public class StartYawlCaseJob implements Job {
 	public static final String MAP_KEY_SPEC_ID = "specId";
-	public static final String MAP_KEY_CASE_ID = "caseId";
 
 	private static final boolean DEBUG = false;
 
-	private static final Log LOG = LogFactory.getLog( StartYawlCaseJob.class );
-	
-	public StartYawlCaseJob() {
-	}
-	
-	public void execute( JobExecutionContext context ) throws JobExecutionException {
+	private static final Log LOG = LogFactory.getLog(StartYawlCaseJob.class);
 
-		JobDetail jobDetail = context.getJobDetail();
-		JobDataMap dataMap = jobDetail.getJobDataMap();
-		String specID = dataMap.getString( MAP_KEY_SPEC_ID );
-		
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+
 		String result = "";
-		
+		String specID = context.getTrigger().getJobDataMap().getString(
+				MAP_KEY_SPEC_ID);
 		try {
 			// get the client to the engine
 			InterfaceB_EnvironmentBasedClient clientB = InterfaceB.getClient();
-	    	
-			if( DEBUG ) {
-				LOG.info( clientB.getSpecification( specID, InterfaceB.getConnectionHandle() ) );
+
+			if (DEBUG) {
+				LOG.info(clientB.getSpecification(specID, InterfaceB
+						.getConnectionHandle()));
 			}
 			// launch the case
-			result = clientB.launchCase( specID, "", InterfaceB.getConnectionHandle() );
+			result = clientB.launchCase(specID, "", InterfaceB
+					.getConnectionHandle());
+
+			// try to convert the result into a number. If the case was launched
+			// it will
+			// be an ID and this will succeed, if it wasn't launched this will
+			// fail
+			Long.parseLong(result);
 			context.setResult(result);
-			LOG.debug( result );
-			
-			// try to convert the result into a number. If the case was launched it will
-			// be an ID and this will succeed, if it wasn't launched this will fail
-			long id = Long.parseLong( result );
-		}
-		catch( IOException e ) {
-			LOG.error( "Error starting case for specification '" + specID + "'", e );
-			
-			StringWriter sw = new StringWriter();
-    		e.printStackTrace( new PrintWriter( sw ) );
-		}	
-		catch( NumberFormatException e ) {
-			LOG.error( "Error starting case for specification '" + specID + "'", e );
+		} catch (IOException e) {
+			System.out.println(e);
+			throw new JobExecutionException(
+					"Error starting case for specification '" + specID + "'",
+					e, false);
+		} catch (NumberFormatException e) {
+			System.out.println(e);
+			throw new JobExecutionException(
+					"Error starting case for specification '" + specID + "'\n"
+							+ result);
 		}
 	}
 }
