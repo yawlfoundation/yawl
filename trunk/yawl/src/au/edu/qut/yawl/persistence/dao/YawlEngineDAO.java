8ebbbacca093918e50ffb20c907326dae6bdd790
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-16 21:38:32 +0000

    additions to illustrate desired retries behavior

--- trunk/yawl/src/au/edu/qut/yawl/persistence/dao/YawlEngineDAO.java
+++ trunk/yawl/src/au/edu/qut/yawl/persistence/dao/YawlEngineDAO.java
@@ -1,10 +1,18 @@
 package au.edu.qut.yawl.persistence.dao;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Constructor;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -14,6 +22,8 @@ import org.jdom.JDOMException;
 import au.edu.qut.yawl.elements.YSpecification;
 import au.edu.qut.yawl.engine.interfce.InterfaceA_EnvironmentBasedClient;
 import au.edu.qut.yawl.engine.interfce.InterfaceB_EnvironmentBasedClient;
+import au.edu.qut.yawl.engine.interfce.Interface_Client;
+import au.edu.qut.yawl.exceptions.YAuthenticationException;
 import au.edu.qut.yawl.exceptions.YPersistenceException;
 import au.edu.qut.yawl.exceptions.YSchemaBuildingException;
 import au.edu.qut.yawl.exceptions.YSyntaxException;
@@ -23,23 +33,33 @@ import au.edu.qut.yawl.unmarshal.YMarshal;
 
 public class YawlEngineDAO implements DAO {
 
-	InterfaceA_EnvironmentBasedClient iaClient;
+	private InterfaceA_EnvironmentBasedClient iaClient;
 
-	InterfaceB_EnvironmentBasedClient ibClient;
+	private InterfaceB_EnvironmentBasedClient ibClient;
 
-	String sessionAHandle;
+	private String sessionAHandle;
 
-	String sessionBHandle;
+	private String sessionBHandle;
+	
+	private String engineUri = "http://localhost:8080/yawl/";
+	
+	private String user = "admin";
+	
+	private String password = "YAWL";
 
-	public YawlEngineDAO() throws IOException {
-		iaClient = new InterfaceA_EnvironmentBasedClient(
-				"http://localhost:8080/yawl/ia");
-		sessionAHandle = iaClient.connect("admin", "YAWL");
-		ibClient = new InterfaceB_EnvironmentBasedClient(
-				"http://localhost:8080/yawl/ib");
-		sessionBHandle = iaClient.connect("admin", "YAWL");
+	public YawlEngineDAO() {
 	}
 
+	public void initialize() throws IOException {
+		iaClient = new InterfaceA_EnvironmentBasedClient(
+				engineUri + "/ia");
+		sessionAHandle = iaClient.connect(user, password);
+		ibClient = new InterfaceB_EnvironmentBasedClient(
+				engineUri + "/ib");
+		sessionBHandle = iaClient.connect(user, password);
+	}
+	
+	
 	public boolean delete(Object object) {
 		try {
 			iaClient.unloadSpecification(object.toString(), sessionAHandle);
@@ -63,7 +83,8 @@ public class YawlEngineDAO implements DAO {
 			if (!filter.endsWith("/")) {
 				filter = filter + "/";
 			}
-			// this method is stolen from hibernatedao but for the next line
+			// this method is stolen from hibernatedao but for the next line...
+			// which needs to be changed to add a restriction regarding uri match	
 			List tmp = retrieveByRestriction(YSpecification.class,
 					new Unrestricted());
 
@@ -100,18 +121,77 @@ public class YawlEngineDAO implements DAO {
 		return null;
 	}
 
+	public Exception getError(String xml) {
+		Exception retval = null;
+		if (!Interface_Client.successful(xml)) {
+			try {
+				String error = URLDecoder.decode(Interface_Client.stripOuterElement(
+						Interface_Client.stripOuterElement(xml)), "UTF-8");
+				InputStream is = new ByteArrayInputStream(((String) error).getBytes());
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				String s = br.readLine();
+				StringTokenizer st = new StringTokenizer(s, ":");
+				String name = st.nextElement().toString();
+				String message = st.nextElement().toString();
+				List<StackTraceElement> list = new ArrayList<StackTraceElement>();
+				while ((s = br.readLine()) != null) {
+					st = new StringTokenizer(s, "(:) ");
+					st.nextElement();
+					String fqName = st.nextElement().toString();
+					int whereislastdot = fqName.lastIndexOf(".");
+					String method = fqName.substring(whereislastdot + 1);
+					String clazz = fqName.substring(0, whereislastdot);
+					String sourceFile = st.nextElement().toString();
+					int lineNumber = -1;
+					try {
+						lineNumber = Integer.parseInt(st.nextElement().toString());
+					} catch (Exception nfe) {
+					}
+					StackTraceElement e = new StackTraceElement(clazz, method, sourceFile, lineNumber);
+					list.add(e);
+				}
+				Constructor c = Class.forName(name).getConstructor(new Class[] {String.class});
+				retval = (Exception) c.newInstance(new Object[] {message});
+				retval.setStackTrace(list.toArray(new StackTraceElement[] {}));
+			} catch (Exception e) {
+				retval = e;
+			}
+		}
+		return retval;
+	}
+	
+	//this is a very rough prototype of the connection logic eventually to be shared by the entire engine dao.	
 	public Object retrieve(Class type, Object key) {
 		YSpecification retval = null;
+		String xml;
+		Exception e;
 		try {
-			String xml = ibClient.getSpecification(key.toString(),
-					sessionBHandle);
-			List l = YMarshal.unmarshalSpecifications(xml, "abc");
-			if (l != null && l.size() == 1) {
-				retval = (YSpecification) l.get(0);
+			if (sessionBHandle == null) {
+				initialize();
 			}
-		} catch (Exception e) {
+			xml = ibClient.getSpecification(key.toString(), sessionBHandle);
+			e = getError(xml);
+			if (e == null) {
+				List l = YMarshal.unmarshalSpecifications(xml, "imported");
+				if (l != null && l.size() == 1) {
+					retval = (YSpecification) l.get(0);
+				}
+			} else {
+				initialize();
+				xml = ibClient.getSpecification(key.toString(), sessionBHandle);
+				e = getError(xml);
+				if (e == null) {
+					List l = YMarshal.unmarshalSpecifications(xml, "imported");
+					if (l != null && l.size() == 1) {
+						retval = (YSpecification) l.get(0);
+					}
+				} else {
+					throw e;
+				}
+			}			
+		} catch (Exception e2) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e2.printStackTrace();
 		}
 		return retval;
 	}
@@ -148,13 +228,33 @@ public class YawlEngineDAO implements DAO {
 	}
 
 	public static void main(String[] args) {
-		try {
 			YawlEngineDAO dao = new YawlEngineDAO();
 			Object o = dao.retrieve(YSpecification.class, "MakeRecordings");
-			System.out.println(o.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			System.out.println(o);
+	}
+
+	public String getEngineUri() {
+		return engineUri;
+	}
+
+	public void setEngineUri(String engineUri) {
+		this.engineUri = engineUri;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
 	}
 
 }
