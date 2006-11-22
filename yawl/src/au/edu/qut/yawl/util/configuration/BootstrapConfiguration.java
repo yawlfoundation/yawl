ba7f891df9a2cf79146df3ccc52f0265c49ea3ad
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-22 23:59:36 +0000

    new way to get a configuration using a pluggable factory

---
+++ yawl/src/au/edu/qut/yawl/util/configuration/BootstrapConfiguration.java
@@ -0,0 +1,42 @@
+package au.edu.qut.yawl.util.configuration;
+
+import org.springframework.context.ApplicationContext;
+
+public class BootstrapConfiguration {
+
+	private static BootstrapConfiguration instance = new BootstrapConfiguration(false);
+	
+    protected BootstrapConfiguration()  
+    {  
+        if (getClass().equals(BootstrapConfiguration.class))  
+        {  
+            throw new RuntimeException("Violation of intended use, use getInstance() instead");  
+        }  
+    }  
+
+    private BootstrapConfiguration(boolean b) {}  
+    
+	protected ApplicationContext applicationContext;
+	
+	public synchronized static BootstrapConfiguration getInstance() {
+		if (instance == null) {
+			instance = new BootstrapConfiguration();
+		}
+		return instance;
+	}
+
+	public static final synchronized void setInstance(
+			BootstrapConfiguration instance) {
+		if (instance != null
+				&& !instance.getClass().equals(BootstrapConfiguration.class)) {
+			BootstrapConfiguration.instance = instance;
+		} else {
+			throw new RuntimeException("Violation of intended use.");  
+		}
+	}
+	
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+	
+}
