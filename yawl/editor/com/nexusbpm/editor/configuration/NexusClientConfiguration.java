976100b69362f9a3b474e7acb243f5778cf10f27
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-27 16:05:32 +0000

    Updated to allow correct instantiation

--- yawl/editor/com/nexusbpm/editor/configuration/NexusClientConfiguration.java
+++ yawl/editor/com/nexusbpm/editor/configuration/NexusClientConfiguration.java
@@ -18,7 +18,7 @@ public class NexusClientConfiguration extends BootstrapConfiguration{
 	private String YAWL_ENVIRONMENT_PROPERTY_FILE_NAME_VALUE = "editor.properties";
 	private String APPLICATION_CONTEXT_FILE_NAME = "YawlClientApplicationContext.xml";
 	private String YAWL_ENVIRONMENT_PROPERTY_FILE_NAME_KEY = "yawl.environment.propertyfilename";
-
+	
 	public NexusClientConfiguration() {
 		super();
 		System.setProperty(YAWL_ENVIRONMENT_PROPERTY_FILE_NAME_KEY, YAWL_ENVIRONMENT_PROPERTY_FILE_NAME_VALUE);
@@ -39,6 +39,9 @@ public class NexusClientConfiguration extends BootstrapConfiguration{
 	public void saveProperties() throws IOException{
 		UpdateablePropertiesFactoryBean b = (UpdateablePropertiesFactoryBean) applicationContext.getBean("propertyConfigurer");
 		b.save();
+	}
+	public synchronized static BootstrapConfiguration getInstance() {
+			return new NexusClientConfiguration();
 	}
 
 }
