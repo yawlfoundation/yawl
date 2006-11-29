174a1a111ac5798c0bd621c4f6da51aa5f7872a1
nathan_rose <nathan_rose@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2006-11-29 22:29:58 +0000

    added testing for the retrieve-by-restriction changes for the EngineGateway

--- yawl/test/au/edu/qut/yawl/engine/EngineClearer.java
+++ yawl/test/au/edu/qut/yawl/engine/EngineClearer.java
@@ -5,15 +5,17 @@
  * individuals and organisations who are commited to improving workflow technology.
  *
  */
-
-
 package au.edu.qut.yawl.engine;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
+import au.edu.qut.yawl.elements.YSpecification;
 import au.edu.qut.yawl.elements.state.YIdentifier;
 import au.edu.qut.yawl.exceptions.YPersistenceException;
+import au.edu.qut.yawl.persistence.dao.restrictions.Unrestricted;
+import au.edu.qut.yawl.persistence.managed.DataProxy;
 
 /**
  * @author Lachlan Aldred
@@ -21,6 +23,11 @@ import au.edu.qut.yawl.exceptions.YPersistenceException;
  * Time: 17:42:40
  */
 public class EngineClearer {
+	/**
+	 * <em>Warning:</em> This method will clear the engine and its data context! If
+	 * the engine is connected to a database, the database will have all specifications
+	 * removed!
+	 */
     public static void clear(AbstractEngine engine) throws YPersistenceException {
         while (engine.getSpecIDs().iterator().hasNext()) {
             String specID = (String) engine.getSpecIDs().iterator().next();
@@ -31,5 +38,11 @@ public class EngineClearer {
             }
             engine.removeSpecification(specID);
         }
+        
+    	List<DataProxy> list = AbstractEngine.getDataContext().retrieveByRestriction(
+    			YSpecification.class, new Unrestricted(), null );
+    	for( DataProxy proxy : list ) {
+    		AbstractEngine.getDataContext().delete( proxy );
+    	}
     }
 }
