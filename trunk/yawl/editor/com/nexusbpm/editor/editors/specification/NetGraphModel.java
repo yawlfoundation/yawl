7a4f9700fc56fedb62fd73d674248ebde68224b7
rosen1 <rosen1@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2007-02-07 21:25:46 +0000

    removed the editor code from the YAWL engine repository

--- trunk/yawl/editor/com/nexusbpm/editor/editors/specification/NetGraphModel.java
+++
@@ -1,7 +0,0 @@
-package com.nexusbpm.editor.editors.specification;
-
-import org.jgraph.graph.DefaultGraphModel;
-
-public class NetGraphModel extends DefaultGraphModel {
-
-}
--- trunk/yawl/editor/com/nexusbpm/editor/editors/specification/NetGraphUI.java
+++
@@ -1,9 +0,0 @@
-package com.nexusbpm.editor.editors.specification;
-
-import org.jgraph.plaf.basic.BasicGraphUI;
-
-public class NetGraphUI extends BasicGraphUI {
-
-	private static final long serialVersionUID = 1L;
-
-}
