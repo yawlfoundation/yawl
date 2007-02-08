6c2a104318d95a281d17ea3b4d7866594f7532ab
rosen1 <rosen1@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2007-02-08 01:13:24 +0000

    moved the ClassMap to the util package (previously an inner class of AbstractDelegatedDAO), and removed a duplicate version of the AbstractSpringDAO

--- yawl/src/au/edu/qut/yawl/persistence/dao/DelegatedSpringDAO.java
+++ yawl/src/au/edu/qut/yawl/persistence/dao/DelegatedSpringDAO.java
@@ -7,20 +7,14 @@
  */
 package au.edu.qut.yawl.persistence.dao;
 
-import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.DetachedCriteria;
-import org.springframework.orm.hibernate3.SessionFactoryUtils;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import au.edu.qut.yawl.elements.SpecVersion;
 import au.edu.qut.yawl.elements.YAWLServiceReference;
@@ -40,9 +34,6 @@ import au.edu.qut.yawl.exceptions.YPersistenceException;
 import au.edu.qut.yawl.persistence.DatasourceFolder;
 import au.edu.qut.yawl.persistence.dao.restrictions.PropertyRestriction;
 import au.edu.qut.yawl.persistence.dao.restrictions.Restriction;
-import au.edu.qut.yawl.persistence.dao.restrictions.RestrictionCriterionConverter;
-import au.edu.qut.yawl.persistence.dao.restrictions.RestrictionStringConverter;
-import au.edu.qut.yawl.persistence.dao.restrictions.Unrestricted;
 import au.edu.qut.yawl.persistence.dao.restrictions.PropertyRestriction.Comparison;
 
 public class DelegatedSpringDAO extends AbstractDelegatedDAO {
@@ -65,91 +56,6 @@ public class DelegatedSpringDAO extends AbstractDelegatedDAO {
 		addType( YCaseEvent.class, new YCaseEventSpringDAO() );
 		addType( YAtomicTask.class, new YExternalNetElementSpringDAO() );
 		addType( YCompositeTask.class, new YExternalNetElementSpringDAO() );
-	}
-	
-	private abstract class AbstractSpringDAO<Type> extends HibernateDaoSupport implements DAO<Type> {
-		/**
-		 * Hook for subclassers to take care of any operation necessary before the object is saved.
-		 */
-		protected abstract void preSave( Type object ) throws YPersistenceException;
-		
-		public void save( Type object ) throws YPersistenceException {
-			preSave( object );
-//			if( object instanceof YIdentifier ) {
-//				new Exception("save to session(" 
-//						+ "):" + ((YIdentifier)object).getId()).printStackTrace();
-//			} else {
-//				new Exception("save to session(" 
-//						+ "):" + object).printStackTrace();
-//			}
-
-			getHibernateTemplate().saveOrUpdate( object );
-			getHibernateTemplate().flush();
-		}
-		
-		public final Type retrieve( Class type, Object key ) {
-//			new Exception("retrieve from session(" 
-//					+ "):" + key + ":" + type.getName()).printStackTrace();
-			return (Type) getHibernateTemplate().get( type, (Serializable) key );
-		}
-		
-		public List<Type> retrieveByRestriction( Class type, Restriction restriction ) {
-			DetachedCriteria criteria = DetachedCriteria.forClass( type );
-			
-			/*
-			 * This is used because of a funny hibernate effect
-			 * when using eager fetching, duplicate entries may
-			 * be retrieved. This ensures that only one of each are returned.
-			 * */
-			//criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-			
-			
-			if( ! ( restriction instanceof Unrestricted ) ) {
-	        	criteria.add( RestrictionCriterionConverter.convertRestriction( restriction ) );
-	        } 
-			
-			Set<Type> set = new HashSet<Type>( getHibernateTemplate().findByCriteria( criteria ) );
-			
-//			new Exception("retrieve by restriction from session("
-//					+ "):" + RestrictionStringConverter.restrictionToString(restriction) + ":" + type.getName()).printStackTrace();
-			
-			return new ArrayList<Type>( set );
-		}
-		
-		public final void delete( Type object ) {
-//			if( object instanceof YIdentifier ) {
-//				new Exception("delete from session(" 
-//						+ "):" + ((YIdentifier)object).getId()).printStackTrace();
-//			}
-//			if( object instanceof YNetRunner ) {
-//				YNetRunner runner = (YNetRunner)object;
-//				String errorText = "delete:" + runner.getId() + " from session(";
-//				if (runner.getContainingCompositeTask() != null) {
-//					errorText += " task id: " + runner.getContainingCompositeTask().getID();
-//					for (YNetRunner runner1: runner.getContainingCompositeTask().getRunners()) {
-//						errorText += " runner in task is: " + runner1.getId(); 
-//					}
-//				} else {
-//					errorText += " containing composite task is null";
-//				}
-//				new Exception(errorText).printStackTrace();
-//			}
-			//Type persistedObject = (Type) getHibernateTemplate().get(
-			//		object.getClass(), (Serializable) getKey( object ) );
-			//assert persistedObject != null : "attempting to delete object that wasn't persisted";
-			// TODO which way do we want this?
-//			if( persistedObject == null ) {
-//				return false;
-//			}
-
-
-			getHibernateTemplate().delete( object );
-			getHibernateTemplate().flush();
-		}
-		
-		public List getChildren( Object object ) {
-			return new ArrayList();
-		}
 	}
 	
 	private class IdentifierSpringDAO extends AbstractSpringDAO<YIdentifier> {
