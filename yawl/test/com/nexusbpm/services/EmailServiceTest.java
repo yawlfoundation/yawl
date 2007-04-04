6c44260d5daf6278609cf198c640dcdf9dfb5677
msandoz <msandoz@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2007-04-04 18:19:56 +0000

    added email service test

---
+++ yawl/test/com/nexusbpm/services/EmailServiceTest.java
@@ -0,0 +1,29 @@
+package com.nexusbpm.services;
+
+import java.util.Date;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import junit.framework.TestCase;
+
+import com.nexusbpm.services.email.EmailSenderService;
+
+public class EmailServiceTest extends TestCase {
+
+	public void testSend() {
+		EmailSenderService service = new EmailSenderService();
+		try {
+			service.send(
+					"matthew.sandoz@ichotelsgroup.com", 
+					"", "", 
+					"capsela@capsela.hiw.com", 
+					"test from EmailServiceTest", 
+					"Body for EmailServiceTest sent at " + new Date(), 
+					"capsela.hiw.com");
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
