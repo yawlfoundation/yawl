f11cc711b178c117287a85a2a1c2f33036003eac
lindsayb <lindsayb@7ef693a5-8745-4317-bdb7-6b42e680e9f3>
2007-07-27 01:41:20 +0000

    YAWLEditor 1.5 repository maintenance:   * Something is seriously screwed between my work and home repository images. Deleting the entire source from repository here. The next checking will add the bleeding-edge source from my home repository image.

--- yawleditor/trunk/source/au/edu/qut/yawl/editor/specification/SpecificationSelectionSubscriber.java
+++
@@ -1,7 +0,0 @@
-package au.edu.qut.yawl.editor.specification;
-
-import org.jgraph.event.GraphSelectionEvent;
-
-public interface SpecificationSelectionSubscriber {
-  public void receiveGraphSelectionNotification(int state, GraphSelectionEvent event);
-}
