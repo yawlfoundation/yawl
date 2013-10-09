0d0fc670568f6487146c5c91f5e3fba4bf23a394
Michael Adams <adamsmj@users.noreply.github.com>
2013-10-09 00:07:57 +0000

    standardised copyright headers

--- yawleditor/trunk/source/org/yawlfoundation/yawl/editor/ui/net/utilities/NetPrintUtilities.java
+++ yawleditor/trunk/source/org/yawlfoundation/yawl/editor/ui/net/utilities/NetPrintUtilities.java
@@ -1,24 +1,19 @@
 /*
- * Created on 16/09/2004
- * YAWLEditor v1.01 
+ * Copyright (c) 2004-2013 The YAWL Foundation. All rights reserved.
+ * The YAWL Foundation is a collaboration of individuals and
+ * organisations who are committed to improving workflow technology.
  *
- * @author Lindsay Bradford
- * 
- * 
+ * This file is part of YAWL. YAWL is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * YAWL is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * License along with YAWL. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.yawlfoundation.yawl.editor.ui.net.utilities;
