83a9f057020fb8c36a420b734d0aa6ec1247210e
Michael Adams <adamsmj@users.noreply.github.com>
2015-03-18 23:45:47 +0000

    bugfix (issue 541): rationalise identifiers removing some net elements from spec in some very large, non-complete nets.

---
+++ yawleditor/trunk/source/org/yawlfoundation/yawl/editor/core/identity/SuffixPair.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2004-2015 The YAWL Foundation. All rights reserved.
+ * The YAWL Foundation is a collaboration of individuals and
+ * organisations who are committed to improving workflow technology.
+ *
+ * This file is part of YAWL. YAWL is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation.
+ *
+ * YAWL is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with YAWL. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.yawlfoundation.yawl.editor.core.identity;
+
+/**
+ * @author Michael Adams
+ * @date 17/03/15
+ */
+public class SuffixPair {
+
+    private int x;
+    private int y;
+
+    public SuffixPair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+
+    public int getY() { return y; }
+
+    public String former(String name) { return x == 0 ? name : name + "_" + x; }
+
+    public String latter(String name) { return y == 0 ? name : name + "_" + y; }
+}
